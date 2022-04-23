95f2e1a23450bf25be51a0f72401dc027d2f4b3f
Caligiuri Cauã dos Santos <kalikaua@gmail.com>
2022-04-23 13:34:39 -0300

    feat: add user image into the TaskCard component

--- src/components/taskCard/TaskCard.tsx
+++ src/components/taskCard/TaskCard.tsx
@@ -3,7 +3,14 @@ import CardActions from '@mui/material/CardActions'
 import CardContent from '@mui/material/CardContent'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
+import CardMedia from '@mui/material/CardMedia'
 
+const styleCardMedia = {
+  width: 30,
+  height: 30,
+  borderRadius: 15,
+  marginRight: 1
+}
 export default function TaskCard() {
   return (
     <Card sx={{ marginTop: 1 }}>
@@ -15,8 +22,14 @@ export default function TaskCard() {
           This a brief descripton for the item in this collumn
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
+      <CardActions sx={{ justifyContent: 'space-between' }}>
+        <Button size="small">DETAILS</Button>
+        <CardMedia
+          component="img"
+          sx={styleCardMedia}
+          image="https://images.unsplash.com/photo-1567532939604-b6b5b0db2604?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+          alt="Live from space album cover"
+        />
       </CardActions>
     </Card>
   )
