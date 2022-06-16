652d3d8bd36e1c30ed3ff08368990ff0bdca1794
Caligiuri Cauã dos Santos <kalikaua@gmail.com>
2022-06-16 17:59:18 -0300

    chore: add some icons on screen

--- src/components/taskCard/TaskCard.tsx
+++ src/components/taskCard/TaskCard.tsx
@@ -1,23 +1,26 @@
 import Card from '@mui/material/Card'
 import CardActions from '@mui/material/CardActions'
 import CardContent from '@mui/material/CardContent'
-import Button from '@mui/material/Button'
-import Typography from '@mui/material/Typography'
 import CardMedia from '@mui/material/CardMedia'
+import Typography from '@mui/material/Typography'
 import { TaskCardProps } from './types'
 
+import DensityMediumIcon from '@mui/icons-material/DensityMedium'
+import LowPriorityIcon from '@mui/icons-material/LowPriority'
+import PriorityHighIcon from '@mui/icons-material/PriorityHigh'
+
 const styleCardMedia = {
-  width: 30,
-  height: 30,
-  borderRadius: 15,
+  width: 25,
+  height: 25,
+  borderRadius: 12,
   marginRight: 1
 }
 
 export default function TaskCard(props: TaskCardProps) {
   return (
-    <Card sx={{ marginTop: 1 }}>
+    <Card sx={{ marginTop: 0.5 }} onClick={props.handleClick}>
       <CardContent data-testid="card-content">
-        <Typography gutterBottom variant="h5" component="div">
+        <Typography gutterBottom variant="h6" component="div">
           {props.title}
         </Typography>
         <Typography variant="body2" color="text.secondary" noWrap>
@@ -25,9 +28,9 @@ export default function TaskCard(props: TaskCardProps) {
         </Typography>
       </CardContent>
       <CardActions sx={{ justifyContent: 'space-between' }}>
-        <Button size="small" onClick={props.handleClick}>
-          DETAILS
-        </Button>
+        <PriorityHighIcon color="error" fontSize="small" />
+        <LowPriorityIcon color="primary" fontSize="small" />
+        <DensityMediumIcon color="disabled" fontSize="small" />
         <CardMedia
           component="img"
           sx={styleCardMedia}
