b2c55b2d45397446747ec4755389264bc3a4c2b5
Caligiuri Cauã dos Santos <kalikaua@gmail.com>
2022-06-22 08:31:43 -0300

    refactor: add icons and refactor component

--- src/components/taskCard/TaskCard.tsx
+++ src/components/taskCard/TaskCard.tsx
@@ -1,39 +1,51 @@
+import { CardHeader, Icon, Stack } from '@mui/material'
+import Avatar from '@mui/material/Avatar'
 import Card from '@mui/material/Card'
 import CardActions from '@mui/material/CardActions'
 import CardContent from '@mui/material/CardContent'
-import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
-import CardMedia from '@mui/material/CardMedia'
 import { TaskCardProps } from './types'
+
+import DensityMediumIcon from '@mui/icons-material/DensityMedium'
+import LowPriorityIcon from '@mui/icons-material/LowPriority'
+import PriorityHighIcon from '@mui/icons-material/PriorityHigh'
 
 const styleCardMedia = {
   width: 30,
   height: 30,
-  borderRadius: 15,
-  marginRight: 1
+  borderRadius: 20
 }
 
 export default function TaskCard(props: TaskCardProps) {
   return (
-    <Card sx={{ marginTop: 1 }}>
+    <Card sx={{ marginTop: 0.5 }} onClick={props.handleClick}>
+      <CardHeader
+        title={
+          <Typography variant="body1" color="text.secondary">
+            {props.title}
+          </Typography>
+        }
+        avatar={
+          <Avatar sx={styleCardMedia} sizes="25" src={props.imageAuthor} />
+        }
+      />
       <CardContent data-testid="card-content">
-        <Typography gutterBottom variant="h5" component="div">
-          {props.title}
-        </Typography>
-        <Typography variant="body2" color="text.secondary" noWrap>
+        <Typography variant="body2" color="text.secondary">
           {props.body}
         </Typography>
       </CardContent>
       <CardActions sx={{ justifyContent: 'space-between' }}>
-        <Button size="small" onClick={props.handleClick}>
-          DETAILS
-        </Button>
-        <CardMedia
-          component="img"
-          sx={styleCardMedia}
-          image={props.imageAuthor}
-          alt="Live from space album cover"
-        />
+        <Stack direction="row" alignItems="flex-end" spacing={2}>
+          <Icon color="primary" aria-label="upload picture" component="span">
+            <PriorityHighIcon color="error" />
+          </Icon>
+          <Icon color="primary" aria-label="upload picture" component="span">
+            <LowPriorityIcon color="warning" />
+          </Icon>
+          <Icon color="primary" aria-label="upload picture" component="span">
+            <DensityMediumIcon color="primary" />
+          </Icon>
+        </Stack>
       </CardActions>
     </Card>
   )
